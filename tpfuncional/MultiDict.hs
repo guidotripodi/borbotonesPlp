684e4edd4f5ba4be6aee5f8a376d52cd954c99da
Nicolás Hernandez <nicoh22@hotmail.com>
2018-04-06 22:06:35 -0300

    filterMD

--- tpfuncional/MultiDict.hs
+++ tpfuncional/MultiDict.hs
@@ -64,14 +64,14 @@ tablaDelDesde :: Integer -> Integer -> MultiDict Integer Integer
 tablaDelDesde n a = Entry a (n * a) (tablaDelDesde n (a + 1))
 
 serialize :: (Show a, Show b) => MultiDict a b -> String
-serialize = foldMD "[ ]" (\k v r -> "[" ++ (show k) ++ ":" ++ (show v) ++ "," ++r ++ "]" ) (\k r1 r2 -> "[" ++ (show k) ++ ":" ++ r1 ++ "," ++ r2 ++ "]")
+serialize = undefined
 
 mapMD :: (a->c) -> (b->d) -> MultiDict a b -> MultiDict c d
-mapMD f g = foldMD Nil (\k v r1 -> Entry (f k) (g v) r1) (\k r1 r2 ->  Multi (f k) r1 r2)
+maprD f g = foldMD Nil (\k v r1 -> Entry (f k) (g v) r1) (\k r1 r2 ->  Multi (f k) r1 r2)
 
 --Filtra recursivamente mirando las claves de los subdiccionarios.
 filterMD :: (a->Bool) -> MultiDict a b -> MultiDict a b
-filterMD = undefined
+filterMD p = foldMD Nil (\k v r -> if p k then Entry k v r else r) (\k r1 r2 -> if p k then Multi k r1 r2 else r2)
 
 enLexicon :: [String] -> MultiDict String b -> MultiDict String b
 enLexicon = undefined
