a21472ef1de7e686f5857843d873fe4f48a1a22b
Nicolas <nicoh22@hotmail.com>
2018-04-13 00:52:00 -0300

    Listo?

--- tpfuncional/MultiDict.hs
+++ tpfuncional/MultiDict.hs
@@ -95,6 +95,20 @@ definir (x:xs) v d = (recMD (\ks -> cadena v ks)
 
 obtener :: Eq a => [a] -> MultiDict a b -> Maybe b 
 obtener (x:xs) d = (foldMD (\ks -> Nothing) (fEntry) (fMulti)) d (x:xs)
-  where fEntry k1 v r (k:ks) = if null ks then Nothing else ( if k == k1 && (null ks) then Just v else r)
-        fMulti k1 v r1 r2 (k:ks) = if (k == k1) && (not (null ks)) then r1 else (if (k == k1) && (null ks) then Just v else r2 ) 
+  where fEntry k1 v r (k:ks) =
+            if null ks then 
+                Nothing 
+            else 
+                if k == k1 && (null ks) then 
+                    Just v 
+                else 
+                    r (k:ks)
+        fMulti k1 r1 r2 (k:ks) = 
+            if (k == k1) && (not (null ks)) then 
+                r1 ks 
+            else 
+                if (k == k1) && (null ks) then 
+                    Nothing -- Lista vacia y termine en un multi, no hay valor 
+                else 
+                    r2 (k:ks)  
   --foldMD Nothing (\k v r -> if a == k && as == Nil then v else r) (\k v r1 r2 -> if a == k && as != Nil then r1 else (if a == k && as == Nil then v else r2 ) )
